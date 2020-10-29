65f25a68af53aa58cd8a36d9632c88fef85c4230
Marcelo Martínez <marcelo2m1097@gmail.com>
2020-10-29 17:20:49 -0600

    Database connection created.

--- app.js
+++ app.js
@@ -3,8 +3,11 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const apiRouter = require('./routes/api');
+
+const { connect } = require('./config/database');
+
+connect();
 
 var app = express();
 
@@ -14,7 +17,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/api', apiRouter);
 
 module.exports = app;
